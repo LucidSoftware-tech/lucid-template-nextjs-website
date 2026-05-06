899c4a05d5b07b7eacb7c677c15d749562a42ab3
Baxa1997 <bahridnurullav@gmail.com>
2026-05-06 14:19:26 +0500

    corrections

--- src/app/(marketing)/contact/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Contact",
-  description: "Get in touch with our team. We're here to help and answer any questions.",
-};
-
-export default function ContactLayout({ children }) {
-  return children;
-}
--- src/config/navigation.js
+++ src/config/navigation.js
@@ -4,14 +4,4 @@ export const navigationConfig = [
     href: "/",
     icon: "home",
   },
-  {
-    title: "About",
-    href: "/about",
-    icon: "users",
-  },
-  {
-    title: "Contact",
-    href: "/contact",
-    icon: "mail",
-  },
 ];
